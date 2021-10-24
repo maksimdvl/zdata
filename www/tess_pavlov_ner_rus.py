4a606de413b96040713f8d2daa1cb2587bfa5219
Пчелкин Максим <maksim.dvl@yandex.ru>
2021-10-24 22:49:18 +0300

    Update tess_pavlov_ner_rus.py

--- www/tess_pavlov_ner_rus.py
+++ www/tess_pavlov_ner_rus.py
@@ -4,11 +4,11 @@ import pytesseract
 from deeppavlov import configs, build_model
 
 def ocr_core(img):
+   #config_path = configs.ner.ner_rus
    config_path = configs.ner.ner_rus_bert
-   #ner = build_model(config_path, download=True)
    ner = build_model(config_path, download=True)
 
-   #custom_config = r'-l rus --psm 6'
+   #Распознаем поблочно текст с сохранением координат прямоугольных блоков
    df = pytesseract.image_to_data(img,
                                   config=r'-l rus --psm 6',
                                   output_type=pytesseract.Output.DATAFRAME
@@ -18,11 +18,13 @@ def ocr_core(img):
 
    tokens, tags = ner([df1['text'].to_list()])
    del ner
+   #Добавляем к каждой записи с координатами и распзнаным текстом признак лингвистического анализа
    df1.loc[:, "tok"] = tokens[0]
    df1.loc[:, "tag"] = tags[0]
 
    print(df1.head(3))
 
+   #Отбираем только нужные нам сущности B-PER, I-PER
    df2 = df1[(df1['tag'].isin(["B-PER", "I-PER"]))]
 
    df2 = df2[df2['left'] > 10]
@@ -30,6 +32,7 @@ def ocr_core(img):
    for idx, row in df2[:].iterrows():
       (x, y, w, h) = (row['left'], row['top'], row['width'], row['height'])
       pencil = ImageDraw.Draw(img)
+      #Закрашиваем зеленым прямоугольником ФИО
       pencil.rectangle((x, y, x + w, y + h), fill='green')
 
    #img.save(f'{name_f}_out.jpg')
