0c1d2ea65dc614fbe9724b39ec5eaeda32eaf7fd
Пчелкин Максим <maksim.dvl@yandex.ru>
2021-11-06 19:07:39 +0300

    Update tp.py

--- www/tp.py
+++ www/tp.py
@@ -13,7 +13,9 @@ def images_ocr(images, ner):
    return image_ocr
 
 # В данной функции процесс распознавания, анализа текста и корректировки изображения разбит по этапам
-def ocr_core(img, ner):  
+def ocr_core(img, ner):
+   #blocks.block(img.copy())
+   
    df = ocr(img)
    df_words = df_extract(df) 
   
@@ -62,53 +64,3 @@ def build_ner():
 # конвертируем ПДФ в изображение
 def pdf_to_image(pdf_file):
    return pdf2image.convert_from_path(pdf_file, dpi=300, fmt='png')
-
-def get_contours(image):
-   # удаление цвета
-   gray_image= image[:,:,0]
-
-   # (1) thresholding image
-   ret,thresh_value = cv2.threshold(gray_image,180,255,cv2.THRESH_BINARY_INV)
-
-   # (2) dilating image to glue letter with e/a
-   kernel = np.ones((2,2),np.uint8)    
-   dilated_value = cv2.dilate(thresh_value,kernel,iterations = 1)
-
-   # (3) просмотр контуров
-   contours, hierarchy = cv2.findContours(dilated_value,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-   # (4) поиск координат 
-   coordinates = []
-   for contour in contours:
-      x, y, w, h = cv2.boundingRect(contour)
-      if h> 50 and w>50 and h*w<350000:  
-         coordinates.append((x,y,w,h))
-   return coordinates
-
-def sort2(val):   #helper for sorting by y
-   return val[1]
-
-def recognize_table(image, coordinates):
-   recognized_table = row = []
-   prev_y = 0
-   coordinates.sort() #sort by x
-   coordinates.sort(key = sort2) # sort by y
-   for coord in coordinates:
-      x,y,w,h = coord
-      if y > prev_y+5: #new row if y is changed
-         recognized_table.append(row)
-         row = [] 
-      crop_img = image[y:y+h, x:x+w]
-      recognized_string = pytesseract.image_to_string(crop_img, lang="rus")
-      row.append(recognized_string.replace("\n"," "))
-      prev_y = y
-   return recognized_table
-
-def func(pic):
-   image = numpy.array(pic) #np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n)
-   image = np.ascontiguousarray(image[..., [2, 1, 0]])  
-   contours_coords = get_contours(page)
-   recognize_table(pic, contours_coords)
-      
-
-# Then, after you make your changes to the array, you should be able to do either pic.putdata(pix) or create a new image with Image.fromarray(pix).
